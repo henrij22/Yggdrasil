0fe69def1591db2afd933bbc47426cdfa6152562
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2026-08-10 11:02:28 +0200

    update libcxxwrap_julia_jll

--- T/TinyGismo/build_tarballs.jl
+++ T/TinyGismo/build_tarballs.jl
@@ -52,7 +52,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency(PackageSpec(; name="libjulia_jll")),
-    Dependency("libcxxwrap_julia_jll"; compat="0.14"),
+    Dependency("libcxxwrap_julia_jll"; compat="~0.14.10"),
     Dependency("gismo_jll"; compat="~25.7.0"),
 ]
 
